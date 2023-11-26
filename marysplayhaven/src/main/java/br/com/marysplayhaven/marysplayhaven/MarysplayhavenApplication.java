26bca1fd419b7b1a516401ca195a24b05eeedd80
LaraALVV <85951647+LaraALVV@users.noreply.github.com>
2023-11-26 14:10:43 -0300

    Primeiros passos
    Criação do projeto e dependências

---
+++ marysplayhaven/src/main/java/br/com/marysplayhaven/marysplayhaven/MarysplayhavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.marysplayhaven.marysplayhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarysplayhavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarysplayhavenApplication.class, args);
+	}
+
+}
---
+++ marysplayhaven/src/test/java/br/com/marysplayhaven/marysplayhaven/MarysplayhavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marysplayhaven.marysplayhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarysplayhavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
